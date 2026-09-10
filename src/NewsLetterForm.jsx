61d32e4766aa948d857beb5790f1a83702c1ba3a
vdimitrov65-beep <vdimitrov65@gmail.com>
2026-09-10 15:30:34 +0300

    Update subscribe redirect to brief7

--- src/NewsLetterForm.jsx
+++ src/NewsLetterForm.jsx
@@ -25,7 +25,7 @@ export default function NewsletterForm({ lang }) {
         setEmail("");
         setName("");
         setTimeout(() => {
-          window.location.href = "/brief4.html";
+          window.location.href = "/brief7.html";
         }, 1000);
       } else {
         setStatus("error");
